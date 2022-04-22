e89d9cc1db5d00c0c70c45696c79b7d4e873aa2e
Njuacha <betomis2006@yahoo.com>
2022-04-22 09:32:16 +0200

    feat: add viewbinding library and setup ui for search fragment

--- app/src/main/java/com/njuacha/searchflickr/SearchFragment.kt
+++ app/src/main/java/com/njuacha/searchflickr/SearchFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import com.njuacha.searchflickr.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    var _binding: FragmentSearchBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        // setup the toolbar
+        binding.toolBar.inflateMenu(R.menu.menu_search_frag)
+
+        return binding.root
     }
 
 }
